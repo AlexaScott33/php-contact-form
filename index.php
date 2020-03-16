f9ec38fab2bfb5825e1e735d99ea565f0c885f64
Alexa <arscott4792@gmail.com>
2020-03-16 14:41:27 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Contact form to email</title>
+</head>
+
+<body>
+    <p>Send Email</p>
+
+    <form action="contactform.php" method="post"></form>
+    <input type="text" name="name" placeholder="Name">
+    <input type="text" name="mail" placeholder="Email">
+    <input type="text" name="subject" placeholder="Subject">
+    <textarea name="message"></textarea>
+
+    <button name="submit" type="submit">SEND</button>
+
+
+
+</body>
+
+</html>
